4b3813cd7c37519441bd1a2743b6256db4d569ef
Monster-l-bar <monster.l.bar@gmail.com>
2017-11-15 08:32:58 +0800

    test webfont.js

--- assets/js/webfont.js
+++ assets/js/webfont.js
@@ -1,3 +1,7 @@
+<script type="text/javascript" src="http://cdn.webfont.youziku.com/wwwroot/js/wf/youziku.api.min.js">
+</script>
+<script type="text/javascript">
 $youziku.load("body", "e6009675768a42bfa4bb982952b85c7e", "CTKaiTiSJ");
 /*$youziku.load("#id1,.class1,h1", "e6009675768a42bfa4bb982952b85c7e", "CTKaiTiSJ");*/
 $youziku.draw();
+</script>
