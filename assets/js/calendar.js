05332fde65d2903942ae4be9357c8993ef515fc4
Monster-l-bar <monster.l.bar@gmail.com>
2017-11-15 16:30:19 +0800

    put showCalender to function

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -2,6 +2,7 @@
 var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aguest', 'September', 'Octorber', 'November', 'December'];
 var weekArray = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
 var monthDayArray = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 function isLeap(year){
 	if(year % 400 === 0)
 		return true;
@@ -11,6 +12,7 @@ function isLeap(year){
 		return true;
 	return false;
 }
+
 function getMonthDay(month, year){
 	if(month !== 1){
 		return monthDayArray[month];
@@ -23,51 +25,53 @@ function getMonthDay(month, year){
 
 //定义变量
 var date = new Date();
-var yearFlag = date.getFullYear();
-var monthFlag = date.getMonth();
-var calendarFlag = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-];
+showCalendar(date);
 
-//获取对象
-var timeHeader = document.getElementById('time-header');
-var timeBody = document.getElementById('time-body');
-
-//显示日期
-timeHeader.innerHTML = monthArray[monthFlag] + ' ' + yearFlag;
-date.setDate(1);
-var cycleFlag = 0;
-var weekFlag = date.getDay();
-for(i = 0; i < getMonthDay(monthFlag, yearFlag); i++){
-    calendarFlag[cycleFlag][weekFlag++] = i+1;
-    if(weekFlag > 6){
-        weekFlag = 0;
-        cycleFlag++;
+function showCalendar(showDate){
+    //获取对象
+    var timeHeader = document.getElementById('time-header');
+    var timeBody = document.getElementById('time-body');
+    //定义日历数组
+    var calendarArray = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ];
+    //显示日历头部
+    timeHeader.innerHTML = monthArray[showDate.getMonth()] + ' ' + showDate.getFullYear();
+    //得到日历数组
+    showDate.setDate(1);
+    var cycleFlag = 0;
+    var weekFlag = showDate.getDay();
+    for(i = 0; i < getMonthDay(showDate.getMonth(), showDate.getFullYear()); i++){
+        calendarArray[cycleFlag][weekFlag++] = i+1;
+        if(weekFlag > 6){
+            weekFlag = 0;
+            cycleFlag++;
+        }
     }
-}
-
-//表格插入
-var timeBodyHtml = '';
-var tableHeaderHtml = "<tr>";
-for(i = 0; i < weekArray.length; i++){
-    tableHeaderHtml += "<th>"+weekArray[i]+"</th>";
-}
-tableHeaderHtml += "</tr>";
-timeBodyHtml += tableHeaderHtml;
-
-var tableBodyHtml = '';
-for(i = 0; i < calendarFlag.length; i++){
-    tableBodyHtml += "<tr>";
-    for(j = 0; j < calendarFlag[i].length; j++){
-        tableBodyHtml += "<td>";
-        tableBodyHtml += calendarFlag[i][j];
-        tableBodyHtml += "</td>";
+    //获得日历体部
+    var timeBodyHtml = '';
+    var tableHeaderHtml = "<tr>";
+    for(i = 0; i < weekArray.length; i++){
+        tableHeaderHtml += "<th>"+weekArray[i]+"</th>";
     }
-    tableBodyHtml += "</tr>";
+    tableHeaderHtml += "</tr>";
+    timeBodyHtml += tableHeaderHtml;
+
+    var tableBodyHtml = '';
+    for(i = 0; i < calendarArray.length; i++){
+        tableBodyHtml += "<tr>";
+        for(j = 0; j < calendarArray[i].length; j++){
+            tableBodyHtml += "<td>";
+            tableBodyHtml += calendarArray[i][j];
+            tableBodyHtml += "</td>";
+        }
+        tableBodyHtml += "</tr>";
+    }
+    timeBodyHtml += tableBodyHtml;
+    //显示日历体部
+    timeBody.innerHTML = timeBodyHtml;
 }
-timeBodyHtml += tableBodyHtml;
-timeBody.innerHTML = timeBodyHtml;
